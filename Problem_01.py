80f9628779db7350f1e2bfd1b2c13cdaee6e93b1
Ayush-tech123 <ayush.sharma@avantika.edu.in>
2026-03-28 08:42:52 +0000

    Pushing all again

---
+++ Problem_01.py
@@ -0,0 +1,12 @@
+M = str(input())
+N = int(input())
+
+def calculate_fine(name, days):
+    fine = float(days * 5)
+    if fine > 150:
+        fine == 150
+    print(f"Book: {name}")
+    print(f"Days overdue: {days}")
+    print(f"Fine: Rs. {fine}")
+
+calculate_fine(M, N)
---
+++ Problem_02.py
@@ -0,0 +1,13 @@
+M = str(input())
+N = int(input())
+F = int(input())
+
+def calculate_fine(name, days, fine):
+    fine = float(days * fine)
+    if fine > 150:
+        fine == 150
+    print(f"Book: {name}")
+    print(f"Days overdue: {days}")
+    print(f"Fine: Rs. {fine}")
+
+calculate_fine(M, N, F)
