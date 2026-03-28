1d38a5449dc567ed2750ea66253f3b3c71b9a294
Ayush-tech123 <ayush.sharma@avantika.edu.in>
2026-03-28 09:19:56 +0000

    final commit

--- Problem_03.py
+++ Problem_03.py
@@ -4,7 +4,7 @@ N = int(input())
 def calculate_fine(name, days):
     fine = float(days * 5)
     if fine > 150:
-        fine = 150
+        fine = 150.0
     print(f"Book: {name}")
     print(f"Days overdue: {days}")
     print(f"Fine: Rs. {fine}")
